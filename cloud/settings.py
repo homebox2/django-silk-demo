114ca578b6dfac9b0a92ba43f97a79fa24754b8c
sean <homebox0518@gmail.com>
2019-01-20 02:16:25 +0800

    add silkdemo app

--- cloud/settings.py
+++ cloud/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloud.silkdemo',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_silk_demo.urls'
+ROOT_URLCONF = 'cloud.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_silk_demo.wsgi.application'
+WSGI_APPLICATION = 'cloud.wsgi.application'
 
 
 # Database
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'django_silk_demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'developer',
+        "USER": "admin",
+        "PASSWORD": "admin"
     }
 }
 
